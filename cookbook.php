78f7e820cbf993e53c0a065a6152930b2b6472b9
Felipe de Oliveira <felipe.auol@gmail.com>
2017-09-15 19:38:07 -0400

    Fixed ingredient display

--- cookbook.php
+++ cookbook.php
@@ -1,6 +1,6 @@
 <?php
 
-include "function_classes.php";
+include "recipes.php";
 include "render.php";
 
 
@@ -13,11 +13,6 @@ $recipe1->addIngredient("water", 2, "cup");
 $recipe2 = new Recipe();
 $recipe2->setSource("Betty Crocker");
 $recipe2->setTitle("Kombucha");
-
-
-//foreach ($recipe1->getIngredients() as $ing) {
-//    echo "\n" . $ing["amount"] . " " . $ing["measure"] . " " . $ing["item"];
-//}
 
 
 $recipe1->addInstruction("This is my first instruction");
--- render.php
+++ render.php
@@ -6,24 +6,24 @@ class Render{
 			{
 				$output = "";
 				foreach ($ingredients as $ing){
-					$output .= $ing["amount"] . " ". $ing["measure"] . " " . $ing["item"];
-					
+					$output .= $ing["amount"] . " ". $ing["measure"] . " " . $ing["item"] . ", ";
 				}
+				return $output;
 			}
 			public static function displayRecipe($recipe)
 		{
 				$output = "";
 				$output = $recipe->getTitle() . " by " . $recipe->getSource();	
-				$output .= "\n";
+				$output .= "<br/>";
 				$output .= implode(", ", $recipe->getTags());
-				$output .= "\n\n";
-				$output .= self::listIngredients ($recipe->getIngredients());
-				$output .= "\n\n";
-//				foreach($recipe->getInstructions() as $inst){
-//					$output .= "\n" . $inst;
-//				}
+				$output .= "<br/><br/>";
+				$output .= self::listIngredients($recipe->getIngredients());
+				$output .= "<br/><br/>";
+			foreach($recipe->getInstructions() as $inst){
+				$output .= "\n" . $inst;
+			}
 				$output .= $recipe->getYield();
-				
+
 				return $output;
 		}
 }
